008924071723c3d0ed0f4e144009bea20dbe3f7d
Dhika Rizky <deka_r@outlook.com>
2019-03-12 08:41:27 +0700

    Responsiveness fix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="w-screen h-screen flex flex-col md:flex-row font-sans">
-      <div class="w-full md:w-1/2 h-full">
-        <div class="w-full h-1/2 z-0" style="background: url('/img/header.jpg') no-repeat center center; background-size:cover"/>
-        <div class="p-6 relative h-1/2">
+    <div class="md:h-screen flex flex-col md:flex-row font-sans">
+      <div class="w-full md:full md:w-1/2 h-full flex flex-col">
+        <div class="w-full h-64 md:h-1/2" style="background: url('/img/header.jpg') no-repeat center center; background-size:cover"/>
+        <div class="p-6 relative md:h-1/2">
           <g-image class="rounded-full border-10 border-white table mx-auto z-20 -mt-24" src="@/assets/img/portrait.jpg" width="128" height="128"  fit="cover" style="object-fit:cover"/>
           <div class="mt-4">
             <h1 class="font-normal text-4xl text-center text-black leading-none">Dhika Rizky</h1>
             <span class="block text-grey-darkest text-center mt-4 text-xl">A Weeb With Thousand Dreams</span>
             <span class="block text-grey-darkest table mx-auto text-lg mt-4"><span class="flex items-center"><map-pin-icon class="mr-2"/>Kota Bekasi, Indonesia</span></span>
           </div>
-          <div class="pin-b pin-l px-6 w-full mb-6 flex absolute items-center">
-              <div class="flex-grow">
+          <div class="md:pin-b md:pin-l mt-4 md:mt-0 md:px-6 w-full mb-6 flex flex-col-reverse md:flex-row md:absolute items-center">
+              <div class="md:flex-grow mt-2">
                 <g-link class="no-underline text-grey-darkest text-lg mr-4" href="/blog">Blog</g-link>
                 <g-link class="no-underline text-grey-darkest text-lg mr-4" href="/resume">Resume</g-link>
               </div>
@@ -23,7 +23,7 @@
             </div>
         </div>
       </div>
-      <div class="w-full hidden md:w-1/2 bg-green md:flex items-center p-12 h-full">
+      <div class="w-full md:w-1/2 bg-green flex items-center p-12 h-full">
         <p class="text-white text-3xl">
           <span class="text-4xl block mb-4">Halo,<br></span>
           Saya Dhika Rizky, seorang pemrogram web dan wibu yang suka dengan teknologi dan pop kultur Jepang.
