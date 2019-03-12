22321afcf3ef9cbf70b879df319ad6536c480e5e
Dhika Rizky <deka_r@outlook.com>
2019-03-12 20:58:15 +0700

    Integrated Prismic to the project

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,3 +1,7 @@
+const axios = require('axios').default
+const prismic = require('prismic-javascript')
+const PrismicDOM = require('prismic-dom')
+
 // Server API makes it possible to hook into various parts of Gridsome
 // on server-side and add custom data to the GraphQL data layer.
 // Learn more: https://gridsome.org/docs/server-api
@@ -6,7 +10,25 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(store => {
-    // Use the Data store API here: https://gridsome.org/docs/data-store-api
+  api.loadSource(async store => {
+    let url = 'https://dhika-rizky-note.prismic.io/api/v2'
+    const contentType = store.addContentType({
+      typeName: 'Posts',
+      route: 'blog/:slug'
+    })
+    let p = await prismic.api(url)
+    let posts = await p.query('', {
+      pageSize: 1000
+    })
+    for(let item of posts.results){
+      let contentData = PrismicDOM.RichText.asHtml(item.data.content)
+      contentType.addNode({
+        id: item.id,
+        slug: item.uid,
+        title: item.data.title[0].text,
+        date: item.data.time,
+        content: contentData
+      })
+    }
   })
 }
